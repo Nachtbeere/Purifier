5895c97b9d3a462f5e947d80d2adbd5042c197d8
noxowl <nachtbeere@outlook.com>
2020-04-17 23:24:20 +0900

    modify create license to object apply method

--- src/main/kotlin/net/nachtbeere/minecraft/purifier/PurifierServer.kt
+++ src/main/kotlin/net/nachtbeere/minecraft/purifier/PurifierServer.kt
@@ -1,8 +1,13 @@
 package net.nachtbeere.minecraft.purifier
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import io.javalin.Javalin
 import io.javalin.core.JavalinConfig
 import io.javalin.http.Context
+import io.javalin.plugin.json.JavalinJson
+import io.javalin.plugin.json.FromJsonMapper
+import io.javalin.plugin.json.ToJsonMapper
 import io.javalin.plugin.openapi.OpenApiOptions
 import io.javalin.plugin.openapi.OpenApiPlugin
 import io.swagger.v3.oas.models.info.Info
@@ -10,24 +15,33 @@ import io.swagger.v3.oas.models.info.License
 
 class PurifierServer(private val port: Int, val pluginInstance: Purifier)  {
     // TODO: load from config file
+    private var gson: Gson = GsonBuilder().create()
     private var app: Javalin = Javalin.create { config: JavalinConfig ->
         config.defaultContentType = "application/json"
         config.contextPath = "/"
         config.registerPlugin(OpenApiPlugin(getOpenApiOptions()))
+        JavalinJson.fromJsonMapper = object : FromJsonMapper {
+            override fun <T> map(json: String, targetClass: Class<T>) = gson.fromJson(json, targetClass)
+        }
+
+        JavalinJson.toJsonMapper = object : ToJsonMapper {
+            override fun map(obj: Any): String = gson.toJson(obj)
+        }
     }
+
     private val instance: Purifier = pluginInstance
     private val classLoader = Thread.currentThread().contextClassLoader
     private val log = instance.logger
 
     private fun getOpenApiOptions(): OpenApiOptions {
-        val license  = License()
-        license.name = "AGPLv3"
-        license.url = "https://raw.githubusercontent.com/Nachtbeere/Purifier/master/LICENSE"
         val applicationInfo: Info = Info()
             .title("Purifier")
             .version("1.0")
             .description("Purifier Bukkit API")
-            .license(license)
+            .license(License().apply {
+                name="AGPLv3"
+                url="https://raw.githubusercontent.com/Nachtbeere/Purifier/master/LICENSE"
+            })
         return OpenApiOptions(applicationInfo).path("/swagger.json")
     }
 
