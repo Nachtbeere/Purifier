5883c1f898127bb242970b4501683588b48009e1
noxowl <nachtbeere@outlook.com>
2020-05-04 00:17:27 +0900

    remove toString override

--- src/main/kotlin/net/nachtbeere/minecraft/purifier/models/ResponseModels.kt
+++ src/main/kotlin/net/nachtbeere/minecraft/purifier/models/ResponseModels.kt
@@ -7,59 +7,31 @@ open class BaseResponseModel() {
     val timestamp = OffsetDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME)
 }
 
-class CommonResponseModel(val result: String): BaseResponseModel() {
-    override fun toString(): String {
-        return ""
-    }
-}
+class CommonResponseModel(val result: String): BaseResponseModel()
 
-class TokenResponseModel(val token: String?): BaseResponseModel() {
-    override fun toString(): String {
-        return ""
-    }
-}
+class TokenResponseModel(val token: String?): BaseResponseModel()
 
-class ServerInfoModel(
-    val version: String,
-    val basedOn: String,
-    val motd: String,
-    val tps: Int?,
-    val gameMode: String,
-    val currentPlayers: Int,
-    val maxPlayers: Int,
-    val isOnlineMode: Boolean,
-    val isHardcore: Boolean
-): BaseResponseModel() {
-    override fun toString(): String {
-        return ""
-    }
-}
+class ServerInfoModel(val version: String,
+                      val basedOn: String,
+                      val motd: String,
+                      val tps: Int?,
+                      val gameMode: String,
+                      val currentPlayers: Int,
+                      val maxPlayers: Int,
+                      val isOnlineMode: Boolean,
+                      val isHardcore: Boolean): BaseResponseModel()
 
-class ServerSystemInfoModel(
-    val startsAt: String,
-    val uptime: String,
-    val processorLoads: String,
-    val memoryLoads: String
-): BaseResponseModel()
+class ServerSystemInfoModel(val startsAt: String,
+                            val uptime: String,
+                            val processorLoads: String,
+                            val memoryLoads: String): BaseResponseModel()
 
 class UsersModel(val total: Int,
-                 val users: ArrayList<UserModel>): BaseResponseModel() {
-    override fun toString(): String {
-        return ""
-    }
-}
+                 val users: ArrayList<UserModel>): BaseResponseModel()
 
 class SingleUserModel(val user: UserModel): BaseResponseModel()
 
 class CurrentTimeModel(val worldName: String,
-                       val currentTime: GameTimeModel): BaseResponseModel() {
-    override fun toString(): String {
-        return ""
-    }
-}
+                       val currentTime: GameTimeModel): BaseResponseModel()
 
-class GameWorldsModel(val worlds: ArrayList<*>): BaseResponseModel() {
-    override fun toString(): String {
-        return ""
-    }
-}
+class GameWorldsModel(val worlds: ArrayList<*>): BaseResponseModel()
